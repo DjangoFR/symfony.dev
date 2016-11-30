c183519748fbb75b850f4e97d1f2bba4d0bb488d
DjangoFR <dado@dadodesign.de>
2016-11-30 10:17:10 +0100

    fix: problems with storage permissions

--- bin/dockremap.sh
+++ bin/dockremap.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 USER_UID=$(id -u)
 
 ## Check for it
 [ $USER_UID -eq 0 ] && { echo -e "\n\nRun this script as NON ROOT USER, please.\n\n"; exit 1; }
 
+$SCRIPTDIR/stop.sh
+
 sudo id -u dockremap &>/dev/null || sudo useradd dockremap
 
 REMAPCNT="dockremap:$USER_UID:65536"
 sudo bash <<EOF
+sed -i.bak '/^dockremap/ d' /etc/subuid
 echo $REMAPCNT >> /etc/subuid
+sed -i.bak '/^dockremap/ d' /etc/subgid
 echo $REMAPCNT >> /etc/subgid
 echo '{"userns-remap": "dockremap"}' > /etc/docker/daemon.json
+EOF
+
+sudo bash <<EOF
+rm -rf $SCRIPTDIR/../symfony/app/cache/*
+rm -rf $SCRIPTDIR/../storage/mysql/*
+chown $USER.$USER "$SCRIPTDIR/../storage/" -R
+chown $USER.$USER "$SCRIPTDIR/../logs/" -R
 EOF
 
 sudo systemctl daemon-reload
